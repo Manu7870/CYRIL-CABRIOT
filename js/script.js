749a994f2ab9d8664ada70a0fd301aea6cf22ed0
Manu7870 <81039215+Manu7870@users.noreply.github.com>
2021-09-02 11:03:39 +0200

    Batch update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":960},"pages":[{"id":"page-accueil","displayName":"Accueil","link":{"linkType":"LinkTypePage","href":"#!page-accueil"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-mes-vignerons","displayName":"Mes vignerons","link":{"linkType":"LinkTypePage","href":"#!page-mes-vignerons"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-contact","displayName":"contact","link":{"linkType":"LinkTypePage","href":"#!page-contact"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
